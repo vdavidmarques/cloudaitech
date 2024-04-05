5973274b17255a1924e2727f93c5b482b703a0df
vinicius <vdavidmarques@gmail.com>
2024-04-05 12:10:46 -0300

    chore: Updating footer style

--- footer.php
+++ footer.php
@@ -1,12 +1,36 @@
 <footer class="text-white">
-    <div class="bg-red-dark">
-        <div class="container">
-            <h1>footer</h1>
+    <div class="bg-red-dark pb-10 pt-8 lg:py-5">
+        <div class="container grid grid-cols-12 items-center">
+            <div class="col-span-12 lg:col-span-3">
+                <a href="/">
+                    <img src="./src/images/cloudaitech-logo.svg" alt="CloudAi Tech logo" class="w-[9.375rem] h-20 object-contain z-30">
+                </a>
+            </div>
+            <div class="col-span-12 mt-10 lg:mt-0 lg:col-span-9">
+                <nav class="navbar font-neuro text-base tracking-wide">
+                    <ul class="gap-y-5 lg:gap-y-0 lg:gap-x-16 flex flex-col lg:flex-row">
+                        <li>
+                            <a href="/about-us.php">About Us</a>
+                        </li>
+                        <li>
+                            <a href="/services.php" class="cursor-not-allowed">Services</a>
+                        </li>
+                        <li>
+                            <a href="/contact.php">Contact</a>
+                        </li>
+                        <li>
+                            <a href="/portfolio.php">Portfolio</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </div>
-    <div class="bg-red-ultradark">
+    <div class="bg-red-ultradark py-6">
         <div class="container">
-            bottom
+            <h3 class="text-sm">
+              <?php echo date("Y") ?>  - All rights reserved
+            </h3>
         </div>
     </div>
 </footer>
--- header.php
+++ header.php
@@ -1,4 +1,6 @@
 <header class="text-white container flex justify-between items-center h-[6.5625rem]">
-    <img src="./src/images/cloudaitech-logo.svg" alt="CloudAi Tech logo" class="w-auto h-[3.875rem] object-contain z-30">
+    <a href="/">
+        <img src="./src/images/cloudaitech-logo.svg" alt="CloudAi Tech logo" class="w-auto h-[3.875rem] object-contain z-30">
+    </a>
     <?php include './components/navbar.php' ?>
 </header>
