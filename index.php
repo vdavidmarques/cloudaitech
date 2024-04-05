9cd05f4a541813829e1ed54f25603dea268de1aa
vinicius <vdavidmarques@gmail.com>
2024-04-05 11:43:18 -0300

    feature: Adding depositions with swiper slides as component

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <!-- <link rel="stylesheet" href="src/css/output.css"> -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="src/js/tailwindcss.js"></script>
-    <script src="src/js/script.js"></script>
     <link rel="stylesheet" href="src/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <style type="text/tailwindcss">
@@ -17,12 +16,12 @@
             }
               /* 20px */
             .heading-title-xxs{
-                @apply font-neuro lg:text-[1.25rem] lg:leading-9 tracking-wide
+                @apply font-neuro text-sm lg:text-[1.25rem] lg:leading-9 tracking-wide
             }
         
             /* 30px */
             .heading-title-xs{
-                @apply font-neuro lg:text-[1.875rem] lg:leading-8 tracking-wide
+                @apply font-neuro text-lg lg:text-[1.875rem] lg:leading-8 tracking-wide
             }
             
             /* 32px */
@@ -32,7 +31,7 @@
             
             /* 46px */
             .heading-title-md{
-                @apply font-neuro lg:text-[2.875rem] lg:leading-[3.125rem] tracking-wide
+                @apply font-neuro text-2xl lg:text-[2.875rem] lg:leading-[3.125rem] tracking-wide
             }
             
             /* 50px */
@@ -57,14 +56,38 @@
             .heading-text-xl{
                 @apply text-[0.875rem] leading-6 lg:leading-9 lg:text-[1.25rem]
             }
+
+            .button{
+                @apply w-fit h-9 rounded-[0.625rem] uppercase px-5 py-3 flex items-center justify-center heading-title-xxs
+            }
+
+            .button-first{
+                @apply bg-red-dark text-white border border-white hover:bg-red-ultradark transition-colors
+            }
+
+            .navbar ul li a{
+                @apply hover:text-red-dark transition-colors
+            }
+            .bg-swiper-button-navigation{
+                @apply bg-[url('./src/icon/arrow-right-circle.svg')] bg-no-repeat bg-cover
+
+            }
         }
     </style>
-
+     <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
 </head>
-<body class="bg-gradient-to-b from-black to-gray-dark h-screen text-white font-resamitz text-lg">
+<body class="bg-black text-white font-resamitz text-lg">
     <?php include 'header.php' ?>
     <?php include 'components/main-text.php' ?>
     <?php include 'components/text-image.php' ?>
+    <?php include 'components/image-text.php' ?>
+    <?php include 'components/depositions.php' ?>
     <?php include 'footer.php' ?>
+
+    <script type="module" src="src/js/swiper.js"></script>
+    <script src="src/js/script.js"></script>
 </body>
 </html>
---
+++ src/js/swiper.js
@@ -0,0 +1,23 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+import Navigation from 'https://cdn.jsdelivr.net/npm/swiper@11/modules/navigation.min.mjs';
+
+const swiper = new Swiper('.swiper.depositions', {
+    modules: [ Navigation ],
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    spaceBetween: 20,
+    breakpoints: {
+        360: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
